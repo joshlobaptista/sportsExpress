b0880c48c3e6be81211ea88217172d7f97b21ada
Joshua Baptista <joshua.lopesbaptista@gmail.com>
2018-07-07 23:24:36 -0400

    first commit

---
+++ public/main.js
@@ -0,0 +1,74 @@
+var basketball = document.getElementsByClassName("fa-basketball-ball");
+var soccer = document.getElementsByClassName("fa-futbol")
+var trash = document.getElementsByClassName("fa-trash");
+
+Array.from(basketball).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const basketball = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        fetch('messages', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          //what is stringify????
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'basketball':basketball
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+Array.from(soccer).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const soccer = parseFloat(this.parentNode.parentNode.childNodes[7].innerText)
+
+        fetch('messagesDown', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          //what is stringify????
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'soccer':soccer
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
+//every get request reloads the page
